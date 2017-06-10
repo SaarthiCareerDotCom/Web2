3534d9f284e27d347d1b08cb02d396a1f5f0a849
AJAY VERMA <verma.av.ajay@gmail.com>
2017-06-10 21:01:26 +0530

    signin included.

--- routes/register.js
+++ routes/register.js
@@ -1,10 +1,17 @@
 var usersCollection = require('../firebase/connectUsers');
 
 var register_A_user = function(aUser){
-  usersCollection.push(aUser,function(err){
-    // console.log(data);
-    console.log(err);
+  var aUserId = aUser.ID.toString();
+  usersCollection.child(aUserId).set({
+    name : aUser.name,
+    age : aUser.age
   });
+    // console.log(data);
+  //   if(err)
+  //     console.log(err);
+  //   else
+  //     console.log('user inserted succesfully');
+  // });
 }
 
 module.exports = register_A_user;
--- routes/users.js
+++ routes/users.js
@@ -2,10 +2,22 @@ var express = require('express');
 var router = express.Router();
 
 var register_A_user = require('./register');
+var getUserDetails = require('./signin');
 /* GET users listing. */
 router.post('/register', function(req, res, next) {
   register_A_user(req.body);
   res.send(req.body);
 });
+router.post('/signin',function(req, res, next) {
 
+  var validatedUserId = req.body.ID;
+  // res.send(req.body);
+  getUserDetails(validatedUserId,(userDetails) => {
+    if(userDetails)
+      res.send(userDetails);
+    else
+      res.send('ID is NOT specified.');
+  });
+  // console.log(userDetails);
+});
 module.exports = router;
